7db4b5170f6d5286154f20035901a3721f4a3670
mutantsan <kvaqich@gmail.com>
2023-03-04 17:43:27 +0200

    Remove unused code

--- ckanext/search_schema/helpers.py
+++
@@ -1,9 +0,0 @@
-
-def search_schema_hello():
-    return "Hello, search_schema!"
-
-
-def get_helpers():
-    return {
-        "search_schema_hello": search_schema_hello,
-    }
--- ckanext/search_schema/tests/test_helpers.py
+++
@@ -1,7 +0,0 @@
-"""Tests for helpers.py."""
-
-import ckanext.search_schema.helpers as helpers
-
-
-def test_search_schema_hello():
-    assert helpers.search_schema_hello() == "Hello, search_schema!"
